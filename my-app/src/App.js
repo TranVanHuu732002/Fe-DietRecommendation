30e39bb82dc915883dfdf60aaf9f1917c912d4e0
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-02-20 20:50:48 +0700

    Create Screen Login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
+// import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/Login";
-import Register from "./screens/Register";
+// import Register from "./screens/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeScreen />}></Route>
+        {/* <Route path="/" element={<HomeScreen />}></Route> */}
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
+        {/* <Route path="/register" element={<Register />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
